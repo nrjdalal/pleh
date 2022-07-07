051b1c83be2351dc796dd1e9b0942c493051b08a
Neeraj Dalal <admin@nrjdalal.com>
2022-07-07 12:43:34 +0530

    Thu Jul  7 12:43:34 IST 2022

---
+++ components/Mobile.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+const Mobile = ({ children }) => {
+  return (
+    <>
+      {/* Mobile view */}
+      <div className="sm:hidden">{children}</div>
+
+      {/* 640px view */}
+      <div className="relative mt-4 hidden h-[98%] w-screen items-center justify-center sm:flex">
+        <div className="absolute top-4 aspect-[0.47/1] h-[92%] rounded-3xl" />
+
+        <img className="absolute h-full" src="/md-iPhone-13-Pro.png" alt="" />
+
+        <div
+          id="hideScrollbar"
+          className="z-10 aspect-[121/250] h-[88%] overflow-y-scroll rounded-b-[40px] rounded-t-[1%]"
+        >
+          {children}
+        </div>
+      </div>
+
+      <style jsx>{`
+        #hideScrollbar::-webkit-scrollbar {
+          display: none;
+        }
+        #hideScrollbar {
+          -ms-overflow-style: none;
+          scrollbar-width: none;
+        }
+      `}</style>
+    </>
+  )
+}
+
+export default Mobile
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="h-screen w-screen overflow-x-hidden">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 
 export default MyApp
