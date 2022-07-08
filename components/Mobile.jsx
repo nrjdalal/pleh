2f7dcc35e2e856fb645efd4f371f2bcfaee305ee
Neeraj Dalal <admin@nrjdalal.com>
2022-07-08 18:21:48 +0530

    Fri Jul  8 18:21:48 IST 2022

--- components/Mobile.jsx
+++ components/Mobile.jsx
@@ -5,14 +5,14 @@ const Mobile = ({ children }) => {
       <div className="h-screen w-screen sm:hidden">{children}</div>
 
       {/* 640px view */}
-      <div className="relative mt-4 hidden h-[98%] w-screen items-center justify-center sm:flex">
+      <div className="relative mt-4 hidden h-[98%] w-screen items-center justify-center sm:flex ">
         <div className="absolute top-4 aspect-[0.47/1] h-[92%] rounded-3xl" />
 
         <img className="absolute h-full" src="/md-iPhone-13-Pro.png" alt="" />
 
         <div
           id="hideScrollbar"
-          className="z-10 aspect-[120.5/250] h-[88%] overflow-y-scroll rounded-b-[40px] rounded-t-[1%]"
+          className="z-10 aspect-[120.5/250] h-[88%] overflow-y-scroll rounded-b-[40px] rounded-t-[1%] "
         >
           {children}
         </div>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,7 +5,7 @@ import Mobile from 'components/Mobile' // ~ For mobile development only, remove 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="h-screen w-screen overflow-x-hidden font-dmSans tracking-wide">
+      <div className="h-screen w-screen overflow-x-hidden bg-slate-100 font-dmSans tracking-wide">
         <Mobile>
           <Component {...pageProps} />
         </Mobile>
